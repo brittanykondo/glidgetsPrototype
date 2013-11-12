87f5cb9a082ecb32dfb4f3ec91ca9bf0f18db79b
Brittany Kondo <brittany.kondo@gmail.com>
2013-11-12 14:32:08 -0500

    trying to visual real network data (les mis), algorithm parameters and initial node placement seem to be hard to manually tune

--- Final Project/src/ForceDirectedLayout.java
+++ Final Project/src/ForceDirectedLayout.java
@@ -1,46 +1,51 @@
+import java.util.ArrayList;
+
 //Algorithm from paper: Simple Algorithms for Network Visualization: A Tutorial
 //                      by: Michael J. McGuffin
 
 public class ForceDirectedLayout {
 	
-    public double springRestLength = 50; //Preferred length of an edge, without forces
-    public double repulsiveForceK = 6250; //Between nodes, low: nodes less repulsive
-    public double springK =1; //Low: more flexible edges
+    public double springRestLength = 10; //Preferred length of an edge, without forces
+    public double repulsiveForceK = 10; //Between nodes, low: nodes less repulsive
+    public double springK = 0.4; //Low: more flexible edges
     public double timeStep = 0.04;   
-    public Node [] nodes;
-    public Edge [] edges;
+    public int numIterations = 5; //Number of times to run the force algorithm
+    public ArrayList <Node> nodes;
+    public ArrayList <Edge> edges;
     public int numNodes, numEdges;
     
     /** Constructor for setting the nodes, using the default constants
      * */
-    ForceDirectedLayout(Node [] n, Edge [] e){
+    ForceDirectedLayout(ArrayList<Node> n, ArrayList<Edge> e){
     	this.nodes = n;
-    	this.numNodes = n.length;
+    	this.numNodes = n.size();
     	this.edges = e;
-    	this.numEdges = e.length;
+    	this.numEdges = e.size();
     }
     
     /** Constructor for setting the constants
      * */
-    ForceDirectedLayout(double l, double rK,double sK,double tS,Node [] n,Edge[] e){
+    ForceDirectedLayout(double l, double rK,double sK,double tS,ArrayList<Node> n, ArrayList<Edge> e){
     	this.springRestLength = l;
     	this.repulsiveForceK = rK;
     	this.springK = sK;
     	this.timeStep = tS;    	
     	this.nodes = n;
-    	this.numNodes = n.length;
+    	this.numNodes = n.size();
     	this.edges = e;
-    	this.numEdges = e.length;
+    	this.numEdges = e.size();
     }
     
     /**Generates the layout by assigning positions of nodes
      * */
     public void makeLayout(){
     	randomizePositions();
-    	calculateForces();
+    	for (int i=0;i<this.numIterations;i++){
+    		calculateForces();
+    	}    	
     	for (int i=0;i<this.numNodes-1;i++){
     		
-    		System.out.println("After: "+this.nodes[i].x+" "+this.nodes[i].y);
+    		System.out.println("After: "+this.nodes.get(i).x+" "+this.nodes.get(i).y);
     	}
     }
     
@@ -49,9 +54,9 @@ public class ForceDirectedLayout {
      * */
     public void randomizePositions(){
     	for (int i=0;i<this.numNodes-1;i++){
-    		this.nodes[i].x = (int)(Math.random()*((500-0)+1));
-    		this.nodes[i].y = (int)(Math.random()*((500-0)+1));
-    		System.out.println("Before: "+this.nodes[i].x+" "+this.nodes[i].y);
+    		this.nodes.get(i).x = (int)(Math.random()*((800-0)+1));
+    		this.nodes.get(i).y = (int)(Math.random()*((800-0)+1));    		
+    		System.out.println("Before: "+this.nodes.get(i).x+" "+this.nodes.get(i).y);
     	}
     }
     
@@ -60,17 +65,17 @@ public class ForceDirectedLayout {
     public void calculateForces (){
     	//Re-set the net forces
     	for (int i=0;i<this.numNodes;i++){
-    		this.nodes[i].forceX = 0;
-    		this.nodes[i].forceY = 0;
+    		this.nodes.get(i).forceX = 0;
+    		this.nodes.get(i).forceY = 0;
     	}
     	Node node1, node2;
     	double dx, dy,distanceSquared,distance,repulsiveForce,fx,fy;
     	
     	//Calculate the repulsive forces between all pairs of nodes
     	for (int i =0; i< this.numNodes;i++){
-    	   node1 = this.nodes[i];
+    	   node1 = this.nodes.get(i);
     		for (int j=i+1;j< this.numNodes-1;j++){
-    			node2 = this.nodes[j];
+    			node2 = this.nodes.get(j);
     			dx = node2.x - node1.x;
     		    dy = node2.y - node1.y;
     			if (dx!=0 || dy!=0){
@@ -83,8 +88,8 @@ public class ForceDirectedLayout {
     				node1.forceY = node1.forceY - fy;
     				node2.forceX = node2.forceX + fx;
     				node2.forceY = node2.forceY + fy;
-    				this.nodes[i] = node1;
-    				this.nodes[j] = node2;
+    				this.nodes.set(i, node1);
+    				this.nodes.set(j, node2);
     			}
     		}
     	}
@@ -93,9 +98,9 @@ public class ForceDirectedLayout {
     	
     	//Calculate the spring force between all adjacent pairs of nodes
     	for (int i=0;i<this.numEdges;i++){
-    		currentEdge = this.edges[i];
-    		node1 = this.nodes[currentEdge.node1];
-    		node2 = this.nodes[currentEdge.node2];
+    		currentEdge = this.edges.get(i);
+    		node1 = this.nodes.get(currentEdge.node1);
+    		node2 = this.nodes.get(currentEdge.node2);
     		dx = node2.x - node1.x;
     		dy = node2.y - node1.y;
     		if (dx!=0 || dy!=0){
@@ -107,15 +112,15 @@ public class ForceDirectedLayout {
     			node1.forceY = node1.forceY + fy;
     			node2.forceX = node2.forceX - fx;
     			node2.forceY = node2.forceY - fy;
-    			this.nodes[currentEdge.node1] = node1;
-    			this.nodes[currentEdge.node2] = node2;
+    			this.nodes.set(currentEdge.node1,node1);
+    			this.nodes.set(currentEdge.node2,node2);
     		}
     	}
     	
     	Node currentNode;
     	//Update positions of nodes
     	for (int i=0;i<this.numNodes;i++){
-    		currentNode = this.nodes[i];
+    		currentNode = this.nodes.get(i);
     		dx = this.timeStep * currentNode.forceX;
     		dy = this.timeStep * currentNode.forceY;
     		distanceSquared = dx*dx + dy*dy;
@@ -124,7 +129,7 @@ public class ForceDirectedLayout {
     		dy = dy*distance;
             currentNode.x = (float) (currentNode.x + dx);
             currentNode.y = (float) (currentNode.y + dy);
-            this.nodes[i] = currentNode;
+            this.nodes.set(i,currentNode);
     	}
     }
 }
--- Final Project/src/Test.java
+++ Final Project/src/Test.java
@@ -1,28 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import processing.core.*;
 
 public class Test extends PApplet {
   //public Node testNode;
-	public Node [] nodes;
-	public Edge [] edges;
-	
+	public ArrayList<Node> nodes;
+	public ArrayList<Edge> edges;
+  
+/**Initialize the view, draw the visualization
+   * */
   public void setup() {
-    size(500,500);
+    size(800,800);
     background(0);
-    generateTestNodes();
+    //generateTestNodes();
+    readLesMis();
     ForceDirectedLayout layout = new ForceDirectedLayout(this.nodes,this.edges);
     layout.makeLayout();
     for (int i = 0;i<layout.numNodes;i++){
-    	layout.nodes[i].display();    	
+    	layout.nodes.get(i).display();    	
     }
     
     for (int i = 0;i<layout.numEdges;i++){
-    	layout.edges[i].display(layout.nodes);    	
+    	layout.edges.get(i).display(layout.nodes);    	
     }
     //testNode = new Node(this,10,10,0);
     //testNode.display();
     
   }
-
+ 
+  /**Re-draw the screen in response to a mouse event
+  * */
   public void draw() {
     stroke(255);    
    // testNode.mouseClicked();
@@ -32,20 +42,62 @@ public class Test extends PApplet {
   
   }
   
-  //Creates a test dataset of nodes and edges 
+  /**Creates a test data set of nodes and edges 
+   * Saves the nodes and edges into global variables
+   */
   public void generateTestNodes(){
-	  Node [] nodes = new Node [10];
+	 this.nodes = new ArrayList<Node>();
+	 this.edges = new ArrayList <Edge>();
+	 
 	 for (int i=0;i<10;i++){
-		 nodes[i] = new Node (this,i);
+		 this.nodes.add(new Node (this,i,""));
 	 }
-	 this.nodes = nodes;
-	 Edge [] edges = new Edge [4];
-	 edges[0] = new Edge (this,0,0,1);
-	 edges[1] = new Edge (this,1,0,3);
-	 edges[2] = new Edge (this,2,4,5);
-	 edges[3] = new Edge (this,3,6,8);
-	 this.edges = edges;
+	
+	 
+	 this.edges.add(new Edge (this,0,0,1));
+	 this.edges.add(new Edge (this,1,0,3));
+	 this.edges.add(new Edge (this,2,4,5));
+	 this.edges.add(new Edge (this,3,6,8));	 
 
+  }
+  /**Reads the text file containing the data of the co-occurrence network in Les Miserables
+   * Saves the nodes and edges into global variables
+   * */
+  public void readLesMis(){
+	  String filename = "lesmisShort.txt";
+	  Scanner scan;
+	  int type = 0;
+	  int edgeCounter = 0;
+	  this.nodes = new ArrayList<Node>();
+	  this.edges = new ArrayList <Edge>();
+	  
+	  try {
+			scan = new Scanner(new File(filename));
+			while(scan.hasNext())
+			{
+				// Get the credit hours and quality points and
+	            // determine if the student is on warning. If so,
+	            // write the student data to the output file.
+				String line;
+				line = scan.nextLine();
+				String[] items = line.split(" ");
+				if (items[0].equals("node")){
+					type = 0;
+				}else if (items[0].equals("edge")){
+					type = 1;
+				}else{
+					if (type==0){ //Create a node
+						this.nodes.add(new Node(this,Integer.parseInt(items[0]),items[1]));
+					}else{ //Create an edge
+						this.edges.add(new Edge (this,edgeCounter,Integer.parseInt(items[0]),Integer.parseInt(items[1])));
+						edgeCounter++;
+					}
+				}				
+			}			
+			
+		} catch (FileNotFoundException e) {			
+			e.printStackTrace();
+		}
   }
 }
 
