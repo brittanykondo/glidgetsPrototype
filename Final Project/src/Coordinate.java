e137b335e616b61e2295401f95afd197f980a0e9
Brittany Kondo <brittany.kondo@gmail.com>
2013-11-15 17:35:29 -0500

    Changing the classes to handle time varying network data

---
+++ Final Project/src/Coordinate.java
@@ -0,0 +1,11 @@
+/**Stores coordinate data
+ * */
+public class Coordinate {
+	public float x;
+	public float y;
+	
+	public Coordinate(float x, float y){
+	     this.x = x;
+	     this.y = y;
+	}
+}
--- Final Project/src/Edge.java
+++ Final Project/src/Edge.java
@@ -16,10 +16,19 @@ public class Edge {
       }
       
       void display(ArrayList<Node> nodes){
-    	  parent.stroke(255);  
+    	  parent.stroke(255,200);  
     	  Node n1 = nodes.get(this.node1);
     	  Node n2 = nodes.get(this.node2);
     	  parent.line(n1.x,n1.y,n2.x,n2.y);
+      } 
+      
+      /**Draws an edge at a certain moment in time
+       * */
+      void display(ArrayList<Node> nodes, int view){
+    	  parent.stroke(255);  
+    	  Node n1 = nodes.get(this.node1);
+    	  Node n2 = nodes.get(this.node2);
+    	  parent.line(n1.coords.get(view).x,n1.coords.get(view).y,n2.coords.get(view).x,n2.coords.get(view).y);
       } 
       
      /** void mouseClicked(){
